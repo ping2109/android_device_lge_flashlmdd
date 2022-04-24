630a24d5b760eb6454ab27e3425e27d6139b128a
pingmado <75196272+ping2109@users.noreply.github.com>
2022-04-24 18:25:02 +0700

    flashlmdd: add some extra gaming build flags

--- cherish_flashlmdd.mk
+++ cherish_flashlmdd.mk
@@ -26,7 +26,6 @@ $(call inherit-product, device/lge/flashlmdd/device.mk)
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1440
-TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifiers
 
@@ -50,6 +49,15 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="flashlmdd_lao_com-user 11 RKQ1.210420.001 2122112261c44 release-keys"
 
 BUILD_FINGERPRINT := "lge/flashlmdd_lao_com/flashlmdd:11/RKQ1.210420.001/2122112261c44:user/release-keys"
+
+# Some Build Flags
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+TARGET_USES_AOSP_CHARGER := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
 
 # Add CherishOS Maintainer name
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
